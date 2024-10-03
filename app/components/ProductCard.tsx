38e4e09362714bbbcbff6c6405e45fbb6be6506f
prudorexa <mathuprudence24@gmail.com>
2024-10-03 16:50:16 +0300

    add app

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client'; 
+import React from 'react';
+import AddToCart from './AddToCart';
+
+
+const ProductCard = () => {
+  return (
+    <div className='p-5 my-5 text-white text-xl'>
+        <AddToCart />
+    </div>
+  );
+}
+
+export default ProductCard;
